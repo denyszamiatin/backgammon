6dfa620fe7ce2c13ae67753f2d2cf46872c480b9
oleksandrzinoviev <shurik.zinovyev@gmail.com>
2017-02-17 23:22:34 +0200

    Create game board

--- main.py
+++ main.py
@@ -6,5 +6,20 @@ def score():
 def print_score():
     print(score())
 
+def list_w():
+    for i in range (1,13):
+        list_w = [['w'+str(i)]]
+        print(list_w)
+
+def list_b():
+    for i in range(13,25):
+        list_b = [['b'+str(i)]]
+        print(list_b)
+
+
+def board():
+    return list_w(), list_b()
+
 print_score()
 print_score()
+board()
