b45be8d297686f903e95dacf8a20eb36b4750afb
edison12347 <eduard.chudaikin@gmail.com>
2017-02-18 22:14:58 +0200

    Created alternative board which is bassed on list. It appears, that it can provide better solution for further "trun code" implimentation.

--- main.py
+++ main.py
@@ -16,10 +16,6 @@ def print_score():
     print(get_score())
 
 
-def create_board():
-    return [" _ "] * BOARD_SIZE
-
-
 def input_player_name(number):
     return input("What is the " + number + " player name?\n")
 
@@ -32,6 +28,9 @@ def check_players():
         else:
             print(players[get_first_turn()])
             break
+"""
+def create_board():
+    return [" _ "] * BOARD_SIZE
 
 
 def init_board(board):
@@ -55,6 +54,73 @@ def print_board(board):
     for index in range(HALF_BOARD, 0, -1):
         print('{0:3d}'.format(index), end=''),
     print('\n')
+"""
+
+def create_alt_board():
+
+    alt_board = [[i+1," "," "]for i in range(BOARD_SIZE)]
+    alt_board = alt_board[::-1]
+    return alt_board
+
+
+
+def init_alt_board(alt_board):
+
+    alt_board[0][1] = CHECKERS_QTY
+    alt_board[0][2] = WHITE_CHECKER
+    alt_board[12][1] = CHECKERS_QTY
+    alt_board[12][2] = BLACK_CHECKER
+
+def print_alt_first_half_board(alt_board):
+
+    """
+    Visualize first half of the board
+    """
+
+    i = HALF_BOARD
+
+    while i < BOARD_SIZE:
+        print('{0:4d}'.format(alt_board[::-1][i][0]), end=" ")
+        i += 1
+    print('\n')
+
+    i = HALF_BOARD   # It is unclear why we should initialize i again
+    print(" ", end= " ")
+
+    while i < BOARD_SIZE:
+        if alt_board[::-1][i][1] != " ":
+            print('{0:4s}'.format(str(alt_board[::-1][i][1]) + alt_board[::-1][i][2]), end=" ")
+        else:
+            print('{0:4s}'.format('_'), end=' ')
+        i += 1
+
+    print("\n\n\n")
+
+def print_alt_second_half_board(alt_board):
+    """
+    Visualize second half of the board
+    """
+
+    i = HALF_BOARD
+
+    print(" ", end= " ")
+    while i < BOARD_SIZE:
+        if alt_board[i][1] != " ":
+            print('{0:4s}'.format(str(alt_board[i][1]) + alt_board[i][2]), end=" ")
+
+        else:
+            print('{0:4s}'.format('_'), end=' ')
+        i += 1
+
+    print('\n')
+
+    i = HALF_BOARD # ????
+
+    while i < BOARD_SIZE:
+        print('{0:4d}'.format(alt_board[i][0]), end=" ")
+        i += 1
+    print('\n')
+
 
 def get_first_turn():
     while True:
@@ -70,13 +136,18 @@ def get_first_turn():
 
 #print_score()
 #print_score()
-board = create_board()
-init_board(board)
+#board = create_board()
+#init_board(board)
+
+alt_board = create_alt_board()
+init_alt_board(alt_board)
+print_alt_first_half_board(alt_board)
+print_alt_second_half_board(alt_board)
 
 players = [input_player_name('first'), input_player_name('second')]
 check_players()
 
-print_board(board)
+#print_board(board)
 
 
 def get_dice_result():
